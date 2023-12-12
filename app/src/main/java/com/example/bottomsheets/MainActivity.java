f9dc10918e268289ec96a7393a23db762e3dbc96
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2023-12-11 22:33:10 -0700

    TabLayout

--- app/src/main/java/com/example/bottomsheets/MainActivity.java
+++ app/src/main/java/com/example/bottomsheets/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.bottomsheets;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomAppBar mBottomAppBar;
+    private LinearLayout mBottomSheet;
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        mBottomSheet = findViewById(R.id.bottom_sheet);
+        mBottomAppBar = findViewById(R.id.bottom_app_bar);
+
+        mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        mBottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+        }
 }
